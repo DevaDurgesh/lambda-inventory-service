06f2623292532b6e73eeb76576e86a2c6b78d4ba
Durgesh Rengasamy <durgeshrengasamy@gmail.com>
2024-08-25 10:33:57 +0530

    Initial commit

---
+++ src/main/java/com/lambda/inventory/LambdaInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lambda.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambda/inventory/LambdaInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
